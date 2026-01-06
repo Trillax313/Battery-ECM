bec0ce90f0a398bbf0f31765de1119131b4825bb
Trillax313 <mohamedali.mourt@gmail.com>
2026-01-06 17:42:09 -0500

    added dt to the __init__ function rather than having that outside of the class

--- hppc.py
+++ hppc.py
@@ -7,19 +7,22 @@ log = logger.Logger()
 battery = model.BatteryECM(model.params)
 action = actions.BatteryActions(battery,log)
 
-dt = 1
+
 
 class HPPC():
+    def __init__(self):
+        self.dt = 1
+        
     def run(self, battery, endSOC):
         
         while battery.curr_SOC > endSOC:
             targetSOC = round(battery.curr_SOC, 1) - 0.1
-            action.rest(3600, dt)
-            action.cc_time(1*battery.Q/3600, dt, 10)
-            action.rest(600, dt)
-            action.cc_time(0.75*battery.Q/3600, dt, 10)
-            action.rest(600,dt)
-            action.cc_SOC(1*battery.Q/3600, dt, targetSOC )
+            action.rest(3600, self.dt)
+            action.cc_time(1*battery.Q/3600, self.dt, 10)
+            action.rest(600, self.dt)
+            action.cc_time(0.75*battery.Q/3600, self.dt, 10)
+            action.rest(600,self.dt)
+            action.cc_SOC(1*battery.Q/3600, self.dt, targetSOC )
         
         df = log.csv_saver('HPPC')
         return (df)
